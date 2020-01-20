8ba35aee4168c8d450fb3dde1cc8804a532b3921
Stiward_Solano <13769238+StiwardSolano@users.noreply.github.com>
2020-01-19 21:30:29 -0500

    update server
    test api

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 /* app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-}); */
+*/
+app.get('/*', function(req, res) {
+  res.send('Estas haciendo GET');
+}); 
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
